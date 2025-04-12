c215eb1dd0f6b27b54365eb62a096bcb722933cd
Jainish Prajapati <prajapatijainish@protonmail.com>
2025-04-12 07:04:05 +0530

    Initial Backend Code

---
+++ Backend/PackPal/src/main/java/com/FightClub/PackPal/PackPalApplication.java
@@ -0,0 +1,13 @@
+package com.FightClub.PackPal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackPalApplication.class, args);
+	}
+
+}
---
+++ Backend/PackPal/src/test/java/com/FightClub/PackPal/PackPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FightClub.PackPal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
